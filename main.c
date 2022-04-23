410ad846fe484a35c9a8086aa0bd5a588cbb5cf1
daiz93 <desmond.kpo@gmail.com>
2022-04-23 11:31:22 +0100

    Add firet function principal

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+
+int main (int c)
+{
+	printf("The number is %i "; c);
+	return (c);
+}
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio>
+#include <stdlib>
 
-
-
-
+int main(int c);
+#endif
